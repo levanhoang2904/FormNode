9f4ff2efc41de97090fa21474b9ef28eb2ffa9f8
levanhoang2904 <levanhoang29042002@gmail.com>
2023-11-13 11:23:13 +0700

    update source

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const handlebar = require('express-handlebars')
 const port = 3000
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('combined'))
+app.use (express.urlencoded({extended: true}))
+app.use (express.json())
+
 app.engine('hbs', handlebar.engine({
   extname: '.hbs'
 }))
@@ -13,6 +16,15 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 app.get('/', function (req, res) {
   res.render('new')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port)
